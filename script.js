31cdc8bad1d5f6ffbd698dadcfc65dcd9df935e4
Vítor Pacheco <vitor050401@gmail.com>
2022-01-11 16:08:32 -0300

    iniciando javascript para emitir alerta do login

---
+++ script.js
@@ -0,0 +1,7 @@
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const botao = document.getElementById('botao');
+
+botao.addEventListener ('click', () => {
+    
+})
