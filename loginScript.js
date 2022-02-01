f1ad1640ea6cfdfbe864ecbb186ac374a0f15715
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-31 22:04:55 -0300

    Voltando para corrigir os media query

--- loginScript.js
+++ loginScript.js
@@ -1,16 +1,19 @@
 const BOTAO_LOGIN = document.querySelector('.btn-login');
 
-
-BOTAO_LOGIN.addEventListener('click', () => {
+const LOGIN = () => {
   const EMAIL_HEADER = document.getElementById('email');
   const SENHA_HEADER = document.getElementById('senha');
   if (EMAIL_HEADER.value === 'tryber@teste.com' && SENHA_HEADER.value === '123456') {
-    alert('Olá, Tryber!');
     window.open('./index.html', '_self');
   } else {
     alert('Email ou senha inválidos.');
   }
   EMAIL_HEADER.value = '';
   SENHA_HEADER.value = '';
+}
+
+BOTAO_LOGIN.addEventListener('click', LOGIN);
+
+document.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) LOGIN();
 });
-  
